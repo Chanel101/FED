b3e56123839d0daf9ad5b9490e653c2581d1b0aa
Chanel101 <vercaemstchanel@gmail.com>
2024-09-23 20:26:27 +0200

    css aangepast

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
-console.log("hi");
+const header = document.querySelector('#firstHeader')
+let lastScrollTop = 0;
+let scrollFromTop;
+
+function scrollHide() {
+  scrollFromTop = document.documentElement.scrollTop;
+  if (scrollFromTop > lastScrollTop) {
+    header.classList.remove("headerdown");
+    header.classList.add('headerup');
+  } else if(scrollFromTop < 50){
+    header.classList.remove("headerdown");
+    header.classList.add('headerup');
+  }
+  else {
+    header.classList.remove('headerup');
+    header.classList.add("headerdown");
+  }
+  lastScrollTop = scrollFromTop <= 0 ? 0 : scrollFromTop;
+}
+window.onscroll = function(){scrollHide()};
