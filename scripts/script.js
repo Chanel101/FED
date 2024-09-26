29a2280056625a3dd21671f229885d8675aed3b5
Chanel101 <vercaemstchanel@gmail.com>
2024-09-26 20:58:21 +0200

    automatische carousel

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,11 @@ const header = document.querySelector('#firstHeader')
 const menuButton = document.querySelector('#menuButton')
 const exitButton = document.querySelector('#exitButton')
 const menu = document.getElementById('hamburgermenu')
+const scrollers = document.querySelectorAll(".scroller")
+
 let lastScrollTop = 0;
 let scrollFromTop;
+
 
 function scrollHide() {
   scrollFromTop = document.documentElement.scrollTop;
@@ -29,6 +32,25 @@ function openMenu(){
   } else {
     menu.style.display = "none";
   }
+}
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector('.scroller');
+    const scrollerContent = Array.from(scroller.children);
+
+    scrollerContent.forEach(item => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    })
+  });
 }
 
 menuButton.addEventListener('click', openMenu)
